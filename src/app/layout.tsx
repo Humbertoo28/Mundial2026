8aa0d793c7a2fad581b34dfe1c7c041e3deaa337
powergirlsstech-max <powergirlsstech@gmail.com>
2026-05-12 20:52:36 -0500

    fix: prohibir estrictamente el zoom en safari mediante el viewport meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/Providers";
@@ -20,6 +20,13 @@ export const metadata: Metadata = {
   icons: {
     icon: "https://flagcdn.com/w80/pa.png",
   }
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
